9bc824de32587f8ec2071f245eddaffd539ad59f
Corrie Stroup <Corrie.Stroup@gmail.com>
2024-08-11 20:10:07 +0700

    Currently downloading test doc on server, endpoint function working

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,11 @@ const port = process.env.PORT || 3000;
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const https = require("https");
+const fs = require("fs");
 
 require("dotenv").config();
+const apiKey = process.env.API_KEY;
 
 // Middleware
 app.use(cors());
@@ -22,6 +25,78 @@ app.get("/", (req, res) => {
 app.post("/generate-invoice", (req, res) => {
   try {
     const invoiceData = req.body;
+
+    // !! UNSTABLE TEST
+    function generateInvoice(invoice, filename, success, error) {
+      const postData = JSON.stringify(invoice);
+      const options = {
+        hostname: "invoice-generator.com",
+        port: 443,
+        path: "/",
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+          "Content-Type": "application/json",
+          "Content-Length": Buffer.byteLength(postData),
+        },
+      };
+
+      const file = fs.createWriteStream(filename);
+
+      const req = https.request(options, function (res) {
+        res
+          .on("data", function (chunk) {
+            file.write(chunk);
+          })
+          .on("end", function () {
+            file.end();
+
+            if (typeof success === "function") {
+              success();
+            }
+          });
+      });
+      req.write(postData);
+      req.end();
+
+      if (typeof error === "function") {
+        req.on("error", error);
+      }
+    }
+
+    const testInvoice = {
+      from: "Nikolaus Ltd\n123 Main St\nAustin, TX 78701",
+      to: "Acme, Corp.\n456 Main St\nAustin, TX 78701",
+      currency: "usd",
+      number: "INV-0001",
+      payment_terms: "Auto-Billed - Do Not Pay",
+      items: [
+        {
+          name: "Starter plan monthly",
+          quantity: 1,
+          unit_cost: 99,
+        },
+      ],
+      fields: {
+        tax: "%",
+      },
+      tax: 5,
+      notes: "Thanks for being an awesome customer!",
+      terms:
+        "No need to submit payment. You will be auto-billed for this invoice.",
+    };
+
+    // !! Using testInvoice, should switch to invoiceData
+    generateInvoice(
+      testInvoice,
+      "invoice.pdf",
+      function () {
+        console.log("Saved invoice to invoice.pdf");
+      },
+      function (err) {
+        console.error(err);
+      }
+    );
 
     if (!invoiceData || typeof invoiceData !== "object") {
       return res.status(400).json({ error: "Invalid invoice data" });
